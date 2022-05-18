1816c2fe3801cf46190bbf8fdb76510a7739a5d2
Sark1223 <105814864+Sark1223@users.noreply.github.com>
2022-05-18 13:40:25 -0700

    Add files via upload

---
+++ set_and_get/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace set_and_get
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating = "";
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            this.title = aTitle;
+            this.director = aDirector;
+            Rating = aRating;
+        }
+
+        //method setter and getter
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value == "G" || value == "PG")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie vida = new Movie("La vita e bella", "Roberto", "PG");
+            Movie devoluciones = new Movie("No se aceptan devoluciones", "Eugenio", "Dog");
+
+            //rating = PG,G,PG-13
+
+            Console.WriteLine(devoluciones.Rating);
+            Console.ReadKey();
+        }
+    }
+}
